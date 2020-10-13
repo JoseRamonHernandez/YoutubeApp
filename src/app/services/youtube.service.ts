7cabdbb40e9fc0cac1ba44ae9b74a2bdd1c5687b
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-10-13 15:47:16 -0500

    video 292
    Servicios

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+private youtubeUrl = 'https://www.googleapis.com/youtube/v3';
+
+private apikey = 'AIzaSyADqSE33ulNbtWAAvZbtfbCp5x3_Q_eZY8';
+
+private playlist = 'UUuaPTYj15JSkETGnEseaFFg';
+
+private nextPageToken= '';
+
+  constructor(private http: HttpClient) {}
+
+  getVideos(){
+
+    const url = `${ this.youtubeUrl }/playlistItems`;
+    const params = new HttpParams()
+      .set('part','snippet')
+      .set('maxResults','10')
+      .set('playlistId', this.playlist)
+      .set('Key', this.apikey)
+
+    return this.http.get(url, {params});
+  }
 }

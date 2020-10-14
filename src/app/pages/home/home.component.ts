10672ce4e0b11f33c2bc9dd2659bf072c3b81f9b
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-10-13 20:18:01 -0500

    video 295
    Mostrar un modal con el video

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
 import { Video } from '../../models/youtube.models';
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-home',
@@ -21,5 +23,27 @@ videos: Video[] = []
           console.log(this.videos);
         });
   }
+
+mostrarVideo(video: Video)
+{
+
+  Swal.fire({
+    html: `
+    <h4>${video.title}</h4>
+    <hr>
+    <iframe width="100%"
+    height="315"
+    src="https://www.youtube.com/embed/${video.resourceId.videoId}"
+    frameborder="0"
+    allow="accelerometer;
+    autoplay;
+    clipboard-write;
+    encrypted-media;
+    gyroscope;
+    picture-in-picture"
+    allowfullscreen></iframe>
+    `
+  })
+}
 
 }
